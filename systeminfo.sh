a116556d87b468b05e9f88af93d5a9c61251c7c9
Thenisha-S-ML <Thenisha.S@Xoriant.com>
2024-10-03 09:55:22 +0000

    Add systeminfo.sh

---
+++ systeminfo.sh
@@ -0,0 +1,16 @@
+# Display OS details
+echo "Operating System Details:"
+echo "-------------------------"
+uname -a
+echo ""
+
+# Display RAM stats (free memory)
+echo "Free Memory Stats:"
+echo "--------------------"
+free -h
+echo ""
+
+# Display mounted disks
+echo "Mounted Disks:"
+echo "----------------"
+df -h
